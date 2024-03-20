27e7c85287df1e976dcd5cf17c4b1216715ade3c
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-03-20 19:20:39 +0100

    stash

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,6 +2,12 @@ const MyComponent = () => {
   return (
     <>
       <p>hi !</p>
+      <p>Hello ! </p>
+      <p>hola !</p>
+      <p>Bonjour !</p>
+      <p>hi!</p>
+      <p>My name is Maxime</p>
+      <p>My name is Joe</p>
     </>
   );
 };
